59338330c3fb9071ff24e1e2f84c1ff3ababfa14
lizparody <lizparody@gmail.com>
2017-10-11 11:44:23 -0500

    added the spark library to do the chart and show up the first char

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 
 class WeatherList extends Component {
-  renderWeather(cityData){ //contains the city data
+  renderWeather(cityData){ //contains the city data >list>main>etc...
     const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp); //manipulating the api, we get the list and map over it to get the temp. {main: {temp: 260, humidity: 40, pressure: 55}} is the argument we pass as weather
 
     return (
       <tr key={name}>
         <td>{name}</td>
+        <td>
+          <Sparklines height={120} width={180} data={temps}>
+            <SparklinesLine color="blue" />
+          </Sparklines>
+        </td>
       </tr>
     );
   }
-  
+
 //map to get one city per row
   render(){
     return(
