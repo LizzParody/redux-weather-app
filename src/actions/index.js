6b5b68fb0886c8624c676d5b9a4955f284dd597e
lizparody <lizparody@gmail.com>
2017-10-11 09:22:18 -0500

    manipulating the promise

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,10 +6,12 @@ const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${AP
 export const FETCH_WEATHER = 'FETCH_WEATHER' //we create a variable to keep our action types consistent btwn action creators and our reducers
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
-  const request = axios.get(url)
+  const request = axios.get(url) //axios returns a promise, a promise is a data structure that doesn't contain yet any of our request data
+
+  console.log('Request:', request);
 
   return {
     type: FETCH_WEATHER,
-    payload: request
+    payload: request //we are returning the promise as a payload, but because of redux-promise we are returning the actual data. The reducers don't care about the promise, it cares about the data, so it stops the action and it waits until the promise is resolved and returns the data, it send that to the reducer as the payload
   };
 }
---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,4 @@
+export default function (state = null, action) {
+  console.log('Action received', action); //this is the actual data from the promise
+  return state;
+}
