c09ab540a0521634dc422159ef8aa0748dc0b518
lizparody <lizparody@gmail.com>
2017-10-11 11:15:54 -0500

    created the list weather and shows up the city

---
+++ src/containers/weather_list.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+class WeatherList extends Component {
+  renderWeather(cityData){ //contains the city data
+    const name = cityData.city.name;
+
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+      </tr>
+    );
+  }
+  
+//map to get one city per row
+  render(){
+    return(
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidiy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.weather.map(this.renderWeather)}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+function mapStateToProps({ weather }) { //this is the same as state.weather
+  return { weather } //This is the same as { weather: state.weather } weather from combineReducers reducers/index.js
+}
+
+export default connect(mapStateToProps)(WeatherList);
