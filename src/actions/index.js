027d707605fddec86a97723630eec6d5670ce6af
lizparody <lizparody@gmail.com>
2017-10-11 14:16:41 -0500

    changed the css

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KE
 export const FETCH_WEATHER = 'FETCH_WEATHER' //we create a variable to keep our action types consistent btwn action creators and our reducers
 
 export function fetchWeather(city) {
-  const url = `${ROOT_URL}&q=${city},us`;
+  const url = `${ROOT_URL}&q=${city},co`;
   const request = axios.get(url) //axios returns a promise, a promise is a data structure that doesn't contain yet any of our request data
 
   return {
